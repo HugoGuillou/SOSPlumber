985cc8a40cb7a541e15b8abfcd3e3d65601a5c23
Hugo GUILLOU <hugoguillou@hotmail.fr>
2020-02-01 18:17:48 +0100

    Primitive swipe

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour
@@ -7,50 +8,187 @@ public class Game : MonoBehaviour
     [SerializeField]
     private TextAsset csvFile;
 
-    private List<string> Names;
-    private List<string> Quotes;
+    private Dictionary<string, List<string>> Names;
     private List<string> Genders;
+    private List<string> Quotes;
     private List<string> imagePaths;
+
+    private Queue<Card> Deck;
+    private Card currentCard;
+
+    [SerializeField]
+    private int deckSize = 20;
+
+    [SerializeField]
+    private GameObject cardPrefab;
+
+    [SerializeField]
+    private Transform deckPlaceHolder;
+
+    //swipe detection class
+    private Vector2 firstPressPos;
+    private Vector2 secondPressPos;
+    private Vector2 currentSwipe;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        Names = new List<string>();
+        Names = new Dictionary<string, List<string>>();
+        Names["M"] = new List<string>();
+        Names["F"] = new List<string>();
+        Names["NB"] = new List<string>();
+
         Quotes = new List<string>();
         Genders = new List<string>();
         imagePaths = new List<string>();
 
         RetrieveFromCSV(csvFile);
+        Deck = GenerateDeck(deckSize);
+
+        // Prepare our first card
+        currentCard = Deck.Dequeue();
+        currentCard.GetComponent<SpriteRenderer>().sortingOrder = 1;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Swipe();
+
     }
+
+
+    Queue<Card> GenerateDeck(int size)
+    {
+        Queue<Card> ourDeck = new Queue<Card>();
+
+        for(int i=0; i<size; i++)
+        {
+            GameObject cardGO = Instantiate(cardPrefab, deckPlaceHolder.position, Quaternion.identity, deckPlaceHolder);
+            Card ourCard = cardGO.GetComponent<Card>();
+            ourCard.GetComponent<BoxCollider2D>().enabled = false;
+            ourDeck.Enqueue(ourCard);
+        }
+
+        return ourDeck;
+    }
+
 
     void RetrieveFromCSV(TextAsset path)
     {
         List<string> lines = new List<string>(csvFile.text.Split("\n"[0]));
-        
-        // Ignore 2 first lines of csv (1st is blank, 2nd is field names)
-        lines.RemoveRange(0, 2);
+        lines.RemoveAt(lines.Count - 1);
+        // Ignore first line of csv (1st is field names)
+        lines.RemoveAt(0);
 
         foreach (string line in lines)
         {
-            string[] lineData = line.Split(',');
+            string[] lineData = line.Split(';');
 
-            imagePaths.Add(lineData[1]);
-            Names.Add(lineData[2]);
-            Quotes.Add(lineData[3]);
-            Genders.Add(lineData[4]);
+            string imagePath = lineData[0];
+            string name      = lineData[1];
+            string quote     = lineData[2];
+            string gender    = lineData[3];
 
-            string debugLine = lineData[1] + ", " + lineData[2] + ", " + lineData[3] + ", " + lineData[4];
-            Debug.Log(debugLine);
+            string debugLine = imagePath + ", " + name + ", " + quote + ", " + gender;
+
+            imagePaths.Add(imagePath);
+            Names[gender].Add(gender);
+            Quotes.Add(quote);
 
         }
 
+    }
+
+    //https://forum.unity.com/threads/swipe-in-all-directions-touch-and-mouse.165416/
+    public void Swipe()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //save began touch 2d point
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //save ended touch 2d point
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            //create vector from the two points
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+            //normalize the 2d vector
+            currentSwipe.Normalize();
+
+            /*
+            //swipe upwards
+            if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+        {
+                Debug.Log("up swipe");
+            }
+            //swipe down
+            if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+        {
+                Debug.Log("down swipe");
+            }
+            */
+
+            //swipe left
+            if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+            {
+                SwipeLeft();
+            }
+            //swipe right
+            if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+            {
+                SwipeRight();
+            }
+        }
+    }
+
+    void SwipeRight()
+    {
+        StartCoroutine(sweepRight(currentCard.transform));
+    }
+
+    void SwipeLeft()
+    {
+        StartCoroutine(sweepLeft(currentCard.transform));
+
+    }
+
+    void NextCard()
+    {
+        currentCard = Deck.Dequeue();
+        currentCard.GetComponent<SpriteRenderer>().sortingOrder = 1;
+    }
+
+    IEnumerator sweepRight(Transform tr)
+    {
+        while (tr.position.x <= 700)
+        {
+            tr.Translate(Vector3.right * 80f * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+        Destroy(tr.gameObject);
+        NextCard();
+        Debug.Log("Card gone"); 
+    }
+
+    IEnumerator sweepLeft(Transform tr)
+    {
+        while (tr.position.x >= -700)
+        {
+            tr.Translate(Vector3.left * 80f * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+
+        Destroy(tr.gameObject);
+        NextCard();
+
+        Debug.Log("Card gone");
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Temperature")]
+    [Space]
+
+    [SerializeField]
+    private float maxTemperature = 100;
+    [SerializeField]
+    private float minTemperature = 0;
+    [SerializeField]
+    private float startTemperature = 50;
+    [SerializeField]
+    private float tempDecreaseSpeed = 1;
+
+
+    [Header("House Level")]
+    [Space]
+
+    [SerializeField]
+    private float maxHouseLevel = 100;
+    [SerializeField]
+    private float minHouseLevel = 0;
+    [SerializeField]
+    private float startHouseLevel = 50;
+
+
+    [Header("Player levels")]
+    [Space]
+
+    [SerializeField]
+    private float tempLevel;
+    [SerializeField]
+    private float houseLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempLevel = startTemperature;
+        houseLevel= startHouseLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        houseLevel -= tempDecreaseSpeed * Time.deltaTime;
+
+    }
+}
